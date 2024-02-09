ac40a975c41150ed40090e391a4fb52f731c1213
afosne <62524488+afosne@users.noreply.github.com>
2024-02-10 02:00:13 +0800

    Create gettag.js

---
+++ src/gettag/gettag.js
@@ -0,0 +1,14 @@
+import { Context } from 'telegraf';
+import createDebug from 'debug';
+
+import { name, tag } from 'urls.txt';
+
+const debug = createDebug('bot:gettag_command');
+
+const gettag = () => async (ctx: Context) => {
+  const message = `*${name} ${tag}*\n 订阅地址https://airsub.afosne.workers.dev/?token=afosne&${tag}`;
+  debug(`Triggered "gettag" command with message \n${message}`);
+  await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
+};
+
+export { gettag };
