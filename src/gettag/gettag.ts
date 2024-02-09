9b0791e894f50f4634085a6566c7faca0d0e9304
afosne <62524488+afosne@users.noreply.github.com>
2024-02-10 02:14:14 +0800

    Update gettag.ts

--- src/gettag/gettag.ts
+++ src/gettag/gettag.ts
@@ -1,12 +1,10 @@
 import { Context } from 'telegraf';
 import createDebug from 'debug';
 
-import { name, tag } from './urls.json';
-
 const debug = createDebug('bot:gettag_command');
 
 const gettag = () => async (ctx: Context) => {
-  const message = `*${name} ${tag}*\n 订阅地址https://airsub.afosne.workers.dev/?token=afosne&${tag}`;
+  const message = `*极速云 *\n 订阅地址https://airsub.afosne.workers.dev/?token=afosne&fast`;
   debug(`Triggered "gettag" command with message \n${message}`);
   await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
 };
