15be969d30ca5c2feba1d28fe1bf527922f871ed
afosne <62524488+afosne@users.noreply.github.com>
2024-02-10 02:07:55 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Telegraf } from 'telegraf';
 
 import { about } from './commands';
 import { greeting } from './text';
+import { gettag } from './gettag';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
 
@@ -12,6 +13,7 @@ const bot = new Telegraf(BOT_TOKEN);
 
 bot.command('about', about());
 bot.on('message', greeting());
+bot.command('gettag', gettag());
 
 //prod mode (Vercel)
 export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
